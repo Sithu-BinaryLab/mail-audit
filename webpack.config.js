0bf71589a6f99a278977aee037db21d84822cff1
sithulwin <sithulwin.binarylab@gmail.com>
2025-01-03 14:14:53 +0630

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, 'src', 'index.tsx'),
+  entry: path.join(__dirname, "src", "index.tsx"),
 
   output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, "/dist"),
+    filename: "[name].[contenthash].js",
+    clean: true, // Clean the dist folder before each build
   },
 
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "production", // Switch to production mode for optimizations
+  devtool: "source-map", // Use source maps for debugging production builds
 
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"],
   },
 
   module: {
@@ -22,26 +23,40 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
             presets: [
-              '@babel/preset-env',
-              '@babel/preset-typescript',
-              '@babel/preset-react',
+              "@babel/preset-env",
+              "@babel/preset-typescript",
+              "@babel/preset-react",
             ],
           },
         },
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, './public/index.html'),
-      favicon: './public/favicon.ico',
+      template: path.join(__dirname, "./public/index.html"),
+      favicon: "./public/favicon.ico",
     }),
   ],
-}
+
+  optimization: {
+    splitChunks: {
+      chunks: "all", // Enable code splitting for all chunks
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          chunks: "all",
+        },
+      },
+    },
+  },
+};
